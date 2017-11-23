d9a24efd062b13441c74ae2dab5ff1597dc0d1a7
NuanK <32317211+NuanK@users.noreply.github.com>
2017-11-23 09:59:07 +0800

    Update IGoodsModel.java

--- app/src/main/java/com/bwie/model/IModel/IGoodsModel.java
+++ app/src/main/java/com/bwie/model/IModel/IGoodsModel.java
@@ -3,10 +3,6 @@ package com.bwie.model.IModel;
 import com.bwie.bean.GoodsBean;
 import com.bwie.net.OnNetListener;
 
-/**
- * Created by ASUS on 2017/11/22.
- */
-
 public interface IGoodsModel {
     public void getGoods(OnNetListener<GoodsBean> onNetListener);
 }
