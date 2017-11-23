89e87df3474b13027738fb0c16ed7df93b08a6e6
NuanK <32317211+NuanK@users.noreply.github.com>
2017-11-23 10:00:12 +0800

    Update IGoodsActivity.java

--- app/src/main/java/com/bwie/view/IView/IGoodsActivity.java
+++ app/src/main/java/com/bwie/view/IView/IGoodsActivity.java
@@ -5,9 +5,6 @@ import com.bwie.bean.GoodsBean;
 
 import java.util.List;
 
-/**
- * Created by ASUS on 2017/11/22.
- */
 
 public interface IGoodsActivity {
     public void showList(List<GoodsBean.DataBean>groupList,List<List<GoodsBean.DataBean.DatasBean>>childList);
